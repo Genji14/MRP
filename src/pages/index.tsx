cd66f627acface9c137e35e1d9f98d303aba1277
Genji14 <it.phatvo@gmail.com>
2025-02-03 23:32:29 +0700

    [Chore] Clear Page Content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
 import { GetStaticPropsContext } from 'next'
-import { useTranslations } from 'next-intl'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 export default function Home() {
-  const t = useTranslations('home')
-
-  const { locale, locales, route } = useRouter()
-  const otherLocale = locales?.find((cur) => cur !== locale)
-
   return (
     <>
-      <Link href={route} locale={otherLocale}>
-        {t('switchLocale', { locale: otherLocale })}
-      </Link>
-      <div>This is {t('hello')} page</div>
+      <div>This is home page</div>
     </>
   )
 }
